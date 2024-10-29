09ad9f32f12fe9b985ae4e2f2ad52d82869d1148
Luciano Junior <lusonetto137@outlook.com>
2024-10-29 20:56:11 -0300

    ✨ feat: add CRUD for roles and permissions
    - Created `roleController.js` to manage role-related requests.
    - Added `20241029231109-create-roles.js` migration for creating the `roles` table.
    - Defined `roles.js` model to outline the `roles` schema.
    - Set up `roleRoute.js` to manage CRUD routes for roles.
    - Integrated role routes into `index.js`, updated `usuarioRoute.js` and `produtoRoute.js` to include role-based access.
    - Created `roleService.js` to handle business logic for roles and permissions.

--- api/routes/usuarioRoute.js
+++ api/routes/usuarioRoute.js
@@ -9,8 +9,8 @@ router.use(autenticado)
 router
   .post('/usuarios', UsuarioController.cadastrar)
   .get('/usuarios', UsuarioController.buscarTodosUsuarios)
-  .get('/usuarios/id/:id', UsuarioController.buscarUsuarioPorId)
-  .put('/usuarios/id/:id', UsuarioController.editarUsuario)
-  .delete('/usuarios/id/:id', UsuarioController.deletarUsuario)
+  .get('/usuarios/:id', UsuarioController.buscarUsuarioPorId)
+  .put('/usuarios/:id', UsuarioController.editarUsuario)
+  .delete('/usuarios/:id', UsuarioController.deletarUsuario)
 
 module.exports = router
